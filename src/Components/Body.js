b7fb4c9b4ba1c1c010b17d15704a8aed7d12d1bf
evelyn-tse <eyt7ph@virginia.edu>
2022-10-15 12:10:58 -0400

    Basic header and body added

---
+++ src/Components/Body.js
@@ -0,0 +1,12 @@
+import React from "react";
+import '../Styles/Body.css'; 
+
+const Body = () => {
+  return (
+    <div>
+        <p>test test hello body</p>
+    </div>
+  );
+};
+
+export default Body;
---
+++ src/Components/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+import '../Styles/Header.css'; 
+
+const Header = () => {
+  return (
+    <div>
+        <p>test test hello Header</p>
+    </div>
+  );
+};
+
+export default Header;
