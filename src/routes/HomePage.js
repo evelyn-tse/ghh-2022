d670bde8a25c290298807509b818245b803c4921
Ana Borges <avb5bt@virginia.edu>
2022-10-15 13:59:02 -0400

    have log-in button

--- src/routes/HomePage.js
+++ src/routes/HomePage.js
@@ -1,3 +1,8 @@
+import Button from '@mui/material/Button'
+import { Outlet, Link } from "react-router-dom";
 export default function HomePage(props){
-    return (<><div>HomePage</div></>);
+    return (<>
+    <div>HomePage</div>
+    <Button variant="contained"><Link to={`../login/`}>Log In</Link></Button>
+    </>);
 }
