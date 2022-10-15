c339313837d2614a0d4becd982e62420c119c022
Ana Borges <avb5bt@virginia.edu>
2022-10-15 12:53:48 -0400

    nested routing

--- src/index.js
+++ src/index.js
@@ -17,6 +17,12 @@ const router = createBrowserRouter([
     path: "/",
     element: <Root />,
     errorElement: <ErrorPage />,
+    children: [
+      {
+        path: "contacts/:contactId",
+        element: <Contact />,
+      },
+    ],
     
   },
   {
