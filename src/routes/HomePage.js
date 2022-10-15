b3bbef8a609624ff612ed55f6c25ea04e305c5ed
Ana Borges <avb5bt@virginia.edu>
2022-10-15 16:08:31 -0400

    removed ugly purple line

--- src/routes/HomePage.js
+++ src/routes/HomePage.js
@@ -3,6 +3,6 @@ import { Outlet, Link } from "react-router-dom";
 export default function HomePage(props){
     return (<>
     <div>HomePage</div>
-    <Button variant="contained"><Link to={`../login/`}>Log In</Link></Button>
+    <Button variant="contained"><Link style={{textDecoration: 'none', color: 'white'}} to={`../login/`}>Log In</Link></Button>
     </>);
 }
