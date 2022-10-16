c7c8d220308792d31d9a19cc4616b69bf542d1ff
evelyn-tse <eyt7ph@virginia.edu>
2022-10-16 17:29:54 -0400

    add content to home page

--- src/Components/BudgetInfo/BudgetInfo.js
+++ src/Components/BudgetInfo/BudgetInfo.js
@@ -1,13 +1,16 @@
 import React from "react";
 import MainCard from "../Roi/MainCard.js";
 import image2 from "../Roi/image2.jpeg";
+import big1 from "./big1.jpg";
+import big3 from "./big3.png";
+
 
 
 const BudgetInfo = () => {
   return (
     <div>
-        <MainCard image={image2} imageText={"image2"} title={"Budget Info"} content={"test content text content text content test content text content text content test content text content text content test content text content text content test content text content text content test content text content text content test content text content text content test content text content text content "} linkText={"test linke text"}/>
-        <MainCard image={image2} imageText={"image2"} title={"Budget Info"} content={"test content text content text content test content text content text content test content text content text content test content text content text content test content text content text content test content text content text content test content text content text content test content text content text content "} linkText={"test linke text"}/>
+        <MainCard image={big1} imageText={"big1"} title={"What Is Budgeting?"} content={"'Budgeting is the process of creating a plan to spend your money. This spending plan is called a budget. Creating this spending plan allows you to determine in advance whether you will have enough money to do the things you need to do or would like to do.' - MyMoneyCoach"} linkText={"test linke text"}/>
+        <MainCard image={big3} imageText={"big3"} title={"Why Should I Budget?"} content={"'Budgeting helpsp create financial stability. By tracking expenses and following a plan, a budget makes it easier to pay bills on time, build an emergency fund, and save for major expenses such as a car or home. Overall, a budget puts a person on stronger financial footing for both the day-to-day and the long term.' - Investopedia"} linkText={"test linke text"}/>
     </div>
   );
 };
--- src/Components/HomePage/Home.js
+++ src/Components/HomePage/Home.js
@@ -10,8 +10,8 @@ import big2 from "./big2.jpg";
 const Home = () => {
   return (
     <div>
-        <MainCard image={big1} imageText={"big1"} title={"Welcome!"} content={"test content text content text content test content text content text content test content text content text content test content text content text content test content text content text content test content text content text content test content text content text content test content text content text content "} linkText={"test linke text"}/>
-        <MainCard image={big2} imageText={"big2"} title={"Our Mission"} content={"test content text content text content test content text content text content test content text content text content test content text content text content test content text content text content test content text content text content test content text content text content test content text content text content "} linkText={"test linke text"}/>
+        <MainCard image={big1} imageText={"big1"} title={"Welcome!"} content={"Learn. Explore. Plan Your Future."} linkText={"test linke text"}/>
+        <MainCard image={big2} imageText={"big2"} title={"Our Mission"} content={"Our mission is to help you become a finance expert. We get it: money management is hard. That's why we made this website, so that you have everything you need in one place. Learn first HOW to budget and different investment options, then start budgetting with our budgetting tool. Then check out different investment options and predict how much money you will make based on different rates of returns. Well...what are you waiting for? Go explore!"} linkText={"test linke text"}/>
     </div>
   );
 };
