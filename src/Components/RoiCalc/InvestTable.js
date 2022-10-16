1ca4cb3ca00ca2775fcf217febe51980e1b46e88
Ana Borges <avb5bt@virginia.edu>
2022-10-16 00:40:13 -0400

    figured out the rows of the table

--- src/Components/RoiCalc/InvestTable.js
+++ src/Components/RoiCalc/InvestTable.js
@@ -18,50 +18,24 @@ function createData(id, date, name, shipTo, paymentMethod, amount) {
     return { id, date, name, shipTo, paymentMethod, amount };
 }
 
-const rows = [
-    createData(
-      0,
-      '16 Mar, 2019',
-      'Elvis Presley',
-      'Tupelo, MS',
-      'VISA ⠀•••• 3719',
-      312.44,
-    ),
-    createData(
-      1,
-      '16 Mar, 2019',
-      'Paul McCartney',
-      'London, UK',
-      'VISA ⠀•••• 2574',
-      866.99,
-    ),
-    createData(2, '16 Mar, 2019', 'Tom Scholz', 'Boston, MA', 'MC ⠀•••• 1253', 100.81),
-    createData(
-      3,
-      '16 Mar, 2019',
-      'Michael Jackson',
-      'Gary, IN',
-      'AMEX ⠀•••• 2000',
-      654.39,
-    ),
-    createData(
-      4,
-      '15 Mar, 2019',
-      'Bruce Springsteen',
-      'Long Branch, NJ',
-      'VISA ⠀•••• 5919',
-      212.79,
-    ),
-  ];
 
 
 const InvestTable = ({years, amountMoney, setAmountMoney, startingAmount, setStartingAmount}) => {
 
+  const [rows, setRows] = React.useState([]);
+  const createRows = ()=>{
+    let tempRows = []
+    for (let i = 0; i < years; i++) {
+      tempRows.push({year: i})
+    }
+    tempRows.push({year: years})
+    setRows(tempRows);
+  }
     React.useEffect(()=>{
       if(years < 0 || !years){
         return;
       }
-      alert(years)
+      createRows();
     },[years])
   return (
     <div className="InvestTable">
@@ -103,21 +77,13 @@ const InvestTable = ({years, amountMoney, setAmountMoney, startingAmount, setSta
       <Table size="small">
         <TableHead>
           <TableRow>
-            <TableCell>Date</TableCell>
-            <TableCell>Name</TableCell>
-            <TableCell>Ship To</TableCell>
-            <TableCell>Payment Method</TableCell>
-            <TableCell align="right">Sale Amount</TableCell>
+            <TableCell>Years</TableCell>
           </TableRow>
         </TableHead>
         <TableBody>
           {rows.map((row) => (
             <TableRow key={row.id}>
-              <TableCell>{row.date}</TableCell>
-              <TableCell>{row.name}</TableCell>
-              <TableCell>{row.shipTo}</TableCell>
-              <TableCell>{row.paymentMethod}</TableCell>
-              <TableCell align="right">{`$${row.amount}`}</TableCell>
+              <TableCell>{row.year}</TableCell>
             </TableRow>
           ))}
         </TableBody>
